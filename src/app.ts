39d72dc2c65770d87bb1b32173af0684163114dc
Dylan Munyard <dmunyard@gmail.com>
2022-05-23 21:29:28 +1000

    Season number parsing Find destination path

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
-import ptn from 'parse-torrent-name';
-import { title } from 'process';
+import ptn, { Tv, Movie } from 'parse-torrent-name';
 
 fs.readFile("torrents.txt", 'ascii', (err: NodeJS.ErrnoException | null, data: string) => {
     if (err) {
@@ -12,15 +11,35 @@ fs.readFile("torrents.txt", 'ascii', (err: NodeJS.ErrnoException | null, data: s
     lines.forEach((line: string) => {
         let details = ptn(line);
 
-        // Clean up
-
         // Clear every after season number: expecting <Title> s01 | <Title> s02-s03 | <Title> Season 1
-        details.title = details.title.replace(/s\d+.*/i, "");
-        details.title = details.title.replace(/season \d+.*/i, "");
+        let seasonMatch = details.title.match(/s(?<short>\d{1,2}).*|season (?<long>\d{1,2}).*/i);
+        if (seasonMatch) {
+           const { "short": shortSeason, "long": longSeason } = seasonMatch.groups as { [key: string]: string };
+           (details as Tv).season = parseInt(shortSeason ?? longSeason, 10);
+        }
+
+        details.title = details.title.replace(/s\d{1,2}.*/i, "");
+        details.title = details.title.replace(/season \d{1,2}.*/i, "");
 
         // Clear year numbers from the end of the title: expecting <Title> 2021
         details.title = details.title.replace(/\d{4}$/i, "");
+        
+        // Remove excess whitespace
+        details.title = details.title.trim();
 
-        console.info(details.title);
+        let library = "/media/server";
+        let tv = details as Tv;
+        let movie = details as Movie;
+        if (tv.season) {
+            library = `${library}/tv`;
+            if (tv.episode) {
+                console.info(`mv episode ${tv.episode} to ${library}/${tv.title}/Season 0${tv.season}`);
+            } else {
+                console.info(`mv to ${library}/${tv.title}/Season 0${tv.season}`);
+            }
+        } else {
+            library = `${library}/movies`;
+            console.info(`mv to ${library}/${movie.title}`);
+        }
     });
 }); 
